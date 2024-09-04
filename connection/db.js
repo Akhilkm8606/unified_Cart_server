7731b400eb799f8463c18c34935252272562ba86
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-04 18:57:18 +0530

    .

--- connection/db.js
+++ connection/db.js
@@ -7,7 +7,7 @@ dotenv.config({path: path.resolve(__dirname, 'confiq', 'confiq.env')});
 //connect db
  const connectDB =async ()=>{
 
-    mongoose.connect(process.env.DB_URL,)
+    mongoose.connect(process.env.DB_URLM,)
     .then((res) => console.log(`database connected with${res.connection.host}`))
     .catch((error) => console.log(error.message))
 
