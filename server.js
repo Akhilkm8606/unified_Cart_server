9fd45ca54261f197541ba337e21238cb72afbbb2
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-06 22:21:45 +0530

    s

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ console.log(`RAZORPAY_KEY_SECRET: ${process.env.RAZORPAY_KEY_SECRET}`);
 const app = express();
 connectDB();
 app.use(cors({
-    origin: 'https://unified-cart-client.vercel.app',
+    origin: 'https://unified-cart-client-6ozi.vercel.app',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
 
