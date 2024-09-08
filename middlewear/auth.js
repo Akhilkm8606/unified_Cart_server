8cec0945f3cd5261ac315415d9d6b63944431a62
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-08 23:07:37 +0530

    s

--- middlewear/auth.js
+++ middlewear/auth.js
@@ -1,6 +1,6 @@
+
 const jwt = require('jsonwebtoken');
 
-// Middleware function to verify token
 const verifyToken = (req, res, next) => {
     // Extract token from cookies
     const { token } = req.cookies;
@@ -11,7 +11,7 @@ const verifyToken = (req, res, next) => {
     }
 
     // Verify token
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => { // Corrected `JWT_SECREATE_KEY` to `JWT_SECRET`
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             console.log(err);
             return res.status(401).json({ success: false, message: 'Invalid token.' });
