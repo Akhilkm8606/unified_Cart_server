ff0f6c10b0c46f540b25b98480cbb8e3eca04e72
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-08 23:13:49 +0530

    s

--- middlewear/auth.js
+++ middlewear/auth.js
@@ -2,25 +2,22 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    // Extract token from cookies
-    const { token } = req.cookies;
-
-    // Check if token exists
+    // Extract token from headers
+    const token = req.headers.authorization?.split(' ')[1]; // Extract token from "Bearer <token>"
+  
     if (!token) {
-        return res.status(401).json({ success: false, message: 'Token is required.' });
+      return res.status(401).json({ success: false, message: 'Token is required.' });
     }
-
-    // Verify token
+  
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            return res.status(401).json({ success: false, message: 'Invalid token.' });
-        }
-
-        // Extract user ID from the decoded token and add to request object
-        req.userId = decoded.id; 
-        next(); // Proceed to the next middleware or route handler
+      if (err) {
+        console.log(err);
+        return res.status(401).json({ success: false, message: 'Invalid token.' });
+      }
+  
+      req.userId = decoded.id;
+      next();
     });
-};
-
+  };
+  
 module.exports = verifyToken;
