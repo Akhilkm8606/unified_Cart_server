be294b72868826592da29363d841661b89e4ed44
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-10 10:59:34 +0530

    s

--- middlewear/auth.js
+++ middlewear/auth.js
@@ -1,25 +1,25 @@
-
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1]; // Extract token from "Bearer <token>"
+  let token = req.headers.authorization;
+  
+  if (token && token.startsWith('Bearer ')) {
+    token = token.split(' ')[1]; // Remove 'Bearer ' prefix
+  } else if (req.cookies.token) {
+    token = req.cookies.token; // Get token from cookies
+  }
 
-    if (!token) {
-        return res.status(401).json({ success: false, message: 'Token is required.' });
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Token is required.' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ success: false, message: 'Invalid token.' });
     }
-
-
-    
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            return res.status(401).json({ success: false, message: 'Invalid token.' });
-        }
-
-        req.userId = decoded.id; // Extract the user ID from the token payload
-        next();
-    });
+    req.userId = user.id; // Extract user info from token
+    next();
+  });
 };
 
-  
 module.exports = verifyToken;
